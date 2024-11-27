211310673c629b93d0f297435498f1dfc45767b0
Gabriel Geoffroy <gabriel.geoffroy@hotmail.com>
2024-11-27 18:51:32 -0500

    Added the monster

--- src/Pages/Encyclopedia/Monsters/Monsters.js
+++ src/Pages/Encyclopedia/Monsters/Monsters.js
@@ -1,3 +1,6 @@
+import poisonousMillitank from "../../../Images/Monsters/PoisonousMillitank.jpg";
+import poisonousMillitankSheet from "../../../Images/Monsters/PoisonousMillitankSheet.png";
+
 function Monsters() {
 	return (
 		<div style={{ display: "flex", flexDirection: "column" }}>
@@ -12,6 +15,21 @@ function Monsters() {
 			>
 				<h3>Monsters</h3>
 				<a href="/#encyclopedia">Back to encyclopedia</a>
+			</div>
+			<div>
+				<div style={{ border: "5px solid black" }}>
+					<h4>Poisonous Millitank</h4>
+					<div style={{ display: "flex" }}>
+						<img
+							src={poisonousMillitank}
+							style={{ width: "50%", height: "800px" }}
+						/>
+						<img
+							src={poisonousMillitankSheet}
+							style={{ width: "50%", height: "800px" }}
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
